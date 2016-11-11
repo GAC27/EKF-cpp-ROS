8378ed3281ecaf36c5e9c7ca81f70262feaece3c
Gonçalo Castilho <goncalocastilho@hotmail.com>
2016-11-11 16:06:33 +0000

    did functions for the prediction module

--- src/ekf_implementation/scripts/ekf.py
+++ src/ekf_implementation/scripts/ekf.py
@@ -41,53 +41,156 @@ import rospy
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 
+flag_active_odom = 0
+flag_active_base_scan = 0
+last_odom = 0
+current_odom = 0
+current_base_scan = 0
 
-class EKF:
-    def __init__(self):
-        rospy.init_node('EKF', anonymous=True) #make node 
-        self.flag_active_odom=False
-        self.flag_active_base_scan=False
-        self.pub_base_scan = rospy.Publisher("EKF_base_scan", LaserScan, queue_size=0)
-        self.pub_odom = rospy.Publisher("EKF_odom", Odometry, queue_size=0)
+class Orientation:
+    def __init__(self,x,y,z,w):
+        self.x=x
+        self.y=y
+        self.z=z
+        self.w=w
 
+class Position:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
 
-    def odometryReceived(self,msg):
-         self.last_odom = msg
-         self.flag_active_odom = True
-
-    def BaseScanReceived(self,msg):
-        self.last_base_scan = msg
-        self.flag_active_base_scan = True
-
-    def EKFprocess(self):
-  
-    	print last_odom
-    	print "\n\n\n\n\n\n\n"
-    	print last_base_scan
-    	if(self.flag_active_odom and self.flag_active_base_scan):
-    		self.pub_odom.publish(last_odom)
-    		self.pub_base_scan.publish(last_base_scan)
-    	
-
-    def cleanFlags(self):
-    	self.flag_active_odom = False
-    	self.flag_active_odom = False
+class State:
+    def __init__(self, x=0, y=0, z=0, orientationX=0,orientationY=0,orientationZ=0,orientationW=0):
+        self.position=Position(x,y,z)
+        self.orientation=Orientation(orientationX,orientationY,orientationZ,orientationW)
 
 
 
-    def EKF_algorithm(self):
-        
-        
-        while not rospy.is_shutdown():
-        	rospy.Subscriber('base_scan',LaserScan,BaseScanReceived)
-        	rospy.Subscriber('odom',Odometry,odometryReceived)
 
-        	self.EKFprocess()
-        	self.cleanFlags()
+
+def odometryReceived(msg):
+    global current_odom 
+    global last_odom 
+    last_odom = current_odom
+    current_odom = msg
+    if(last_odom == 0):
+        last_odom = current_odom
+    global flag_active_odom 
+    flag_active_odom = True
+
+def BaseScanReceived(msg):
+    global current_base_scan 
+    current_base_scan = msg
+    global flag_active_base_scan
+    flag_active_base_scan = True
+
+def EKFprocess(currentState):
+    global flag_active_odom
+    global flag_active_base_scan
+    global last_odom
+    global current_odom
+    global current_base_scan
+
+    predictedState=State()
+    predictedObservation=0
+
+    if(flag_active_odom and flag_active_base_scan):
+        pub_odom.publish(current_odom)
+        pub_base_scan.publish(current_base_scan)
+        predictedState=getPredictedState(currentState,getPoseDifference(last_odom.pose.pose,current_odom.pose.pose))
+        #Prediction(predictedState, covariance)
+
+
+    
+
+def cleanFlags():
+    global flag_active_odom 
+    flag_active_odom = False
+    global flag_active_base_scan 
+    flag_active_odom = False
+
+
+
+def EKF():
+    rospy.init_node('EKF', anonymous=True) #make node 
+    #rospy.Subscriber('odom',Odometry,odometryReceived)
+    global pub_base_scan
+    pub_base_scan = rospy.Publisher("EKF_base_scan", LaserScan, queue_size=0)
+    global pub_odom
+    pub_odom = rospy.Publisher("EKF_odom", Odometry, queue_size=0)
+    global flag_active_odom
+    flag_active_odom = False
+    global flag_active_base_scan
+    flag_active_base_scan = False
+    currentState=State()
+
+    while not rospy.is_shutdown():
+        rospy.Subscriber('base_scan',LaserScan,BaseScanReceived)
+        rospy.Subscriber('odom',Odometry,odometryReceived)
+        currentState=EKFprocess(currentState)
+        cleanFlags()
+
+#Receives 2 geometry_msg/Pose Messages
+#Returns a List of floats with position
+#   U(X)
+def getPoseDifference(lastPose,currentPose):
+    deltaPose=[]
+
+    #Position
+    deltaPose.append(currentPose.position.x - lastPose.position.x)
+    deltaPose.append(currentPose.position.y - lastPose.position.y)
+    deltaPose.append(currentPose.position.z - lastPose.position.z)
+
+    #Orientation
+    deltaPose.append(currentPose.orientation.x - lastPose.orientation.x)
+    deltaPose.append(currentPose.orientation.y - lastPose.orientation.y)
+    deltaPose.append(currentPose.orientation.z - lastPose.orientation.z)
+    deltaPose.append(currentPose.orientation.w - lastPose.orientation.w)
+
+    return deltaPose
+
+
+#Returns a new state ~Xk+1 given an action Uk
+def F(Xk,Uk):
+    posX=Xk.position.x + Uk[0]
+    posY=Xk.position.y + Uk[1]
+    posZ=Xk.position.z + Uk[2]
+    
+    orientationX=Xk.orientation.x + Uk[3]
+    orientationY=Xk.orientation.y + Uk[4]
+    orientationZ=Xk.orientation.z + Uk[5]
+    orientationW=Xk.orientation.w + Uk[6]
+
+    return State(posX,posY,posZ,orientationX,orientationY,orientationZ,orientationW)
+
+
+#Returns the error
+def V(Xk):
+    return 0
+
+#Returns a new state ~Xk+1 given an action Uk and applies noise to it
+def getPredictedState(Xk,Uk):
+    newState=F(Xk,Uk)
+    noise=V(Xk)
+    newState.position.x = newState.position.x + noise
+    newState.position.y = newState.position.y + noise
+    newState.position.z = newState.position.z + noise
+
+    newState.orientation.x = newState.orientation.x + noise
+    newState.orientation.y = newState.orientation.y + noise
+    newState.orientation.z = newState.orientation.z + noise
+    newState.orientation.w = newState.orientation.w + noise
+    return newState
+
+
+#TODO
+#def Prediction(state,covariance):
+    #newState = 
+    #TODO
 
 if __name__ == '__main__':
     try:
-        ekf_module = EKF()
-        ekf_module.EKF_algorithm()
+        EKF()
     except rospy.ROSInterruptException:
         pass
