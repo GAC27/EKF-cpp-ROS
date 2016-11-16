5e09c896510eeb9402fd4a85fbcc55c055713aea
Tiago <tiagogomes772@gmail.com>
2016-11-16 11:43:30 +0000

    Starting doing the ekf predition step

--- src/ekf/src/ekf.cpp
+++ src/ekf/src/ekf.cpp
@@ -1,65 +1,112 @@
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
+#include "nav_msgs/Odometry.h"
+#include "ros/ros.h"
+#include "tf/transform_listener.h"
+#include "sensor_msgs/PointCloud.h"
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
+#include "laser_geometry/laser_geometry.h"
+#include <vector>
 
-/**
- * This tutorial demonstrates simple receipt of position and speed of the Evarobot over the ROS system.
- */
 
-/**
- * Callback function executes when new topic data comes.
- * Task of the callback function is to print data to screen.
- */
-void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
+
+
+//Global Variables
+ros::Time begin_time;
+nav_msgs::Odometry::ConstPtr& odom_data;
+nav_msgs::Odometry::ConstPtr& scan_data;
+int counter_steps = 0;
+
+
+class State{
+  float x, y, theta;
+  ros::Time time_now;
+
+  public:
+    State(float x, float y, float theta,  ros::Time time_now){
+      this.x = x;
+      this.y = y;
+      this.theta = theta;
+      this.time_now = time_now;
+    }  
+    std::vector state_vector(){
+      std::vector<float> vec;
+      vec.push_back(x);
+      vec.push_back(y);
+      vec.push_back(theta);
+      return vec;
+    }
+}
+
+void ekf_step(const nav_msgs::Odometry::ConstPtr& msg_odom, const sensor_msgs::LaserScan::ConstPtr& msg_scan, ros::Time time_step){
+  State prev_state;
+  State new_state;
+  //predition step
+  if(counter_steps = 0)
+  {
+    x = msg->pose.pose.position.x;
+    y = msg->pose.pose.position.y;
+    theta = msg->pose.pose.position.x;
+    prev_state = new State()
+  }
+}
+
+
+void odom_receiver(const nav_msgs::Odometry::ConstPtr& msg)
 {
   printf("Seq: [%d]", msg->header.seq);
   printf("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
   printf("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
   printf("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
+  odom_data = msg;
 }
+
+
+void scan_receiver(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+  //TO DO: Base Scan 
+}
+
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "odom_listener");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
+  ros::init(argc, argv, "ekf");   
   ros::NodeHandle n;
+  begin_time = ros::Time::now();   
+  ros::Subscriber sub_odom = n.subscribe("odom", 1000, odom_receiver);
+  ros::Subscriber sub_scan = n.subscribe("base_scan", 1000, scan_receiver);
+  //ros::Subscriber sub_map = n.subscribe("map", 1000, scan_receiver);
+  ros::Publisher pub_new_estimates = n.advertise<std_msgs::String>("different_chatter", 1000);
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  ros::Subscriber sub = n.subscribe("odom", 1000, chatterCallback);
+  ekf_step(sub_odom, sub_scan);
+  counter_steps++;
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
+  ros::Rate loop_rate(10);   
+
+  int count = 0;
+  while (ros::ok())
+  {
+
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+
+    ROS_INFO("%s", msg.data.c_str());
+
+
+    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
   ros::spin();
 
   return 0;
